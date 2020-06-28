a0049f92de2fcd569a2ee73177a7ce77f9336213
akb <doge.wow.so.scare@gmail.com>
2020-06-28 12:06:38 +0900

    style: Add a text editor component (not functional)

---
+++ components/Editor.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-card>
+      <v-card-title>
+        <v-btn>
+          <v-icon>mdi-emoticon-outline</v-icon>
+        </v-btn>
+        <v-btn>
+          <v-icon>mdi-arrow-right-bold-circle-outline</v-icon>
+        </v-btn>
+        <v-btn>
+          <v-icon>mdi-paperclip</v-icon>
+        </v-btn>
+        <v-checkbox
+          v-model="enableEnterToSend"
+          label="Press Enter to send"
+        ></v-checkbox>
+        <v-btn color="primary" class="btn">Send</v-btn>
+      </v-card-title>
+      <v-card-text>
+        <v-textarea outlined no-resize></v-textarea>
+      </v-card-text>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enableEnterToSend: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  text-transform: none; /** Disable conversion to upper cases  */
+}
+</style>
