193beaccd3d6034421c9e3f187198a8a707ff7ac
akb <doge.wow.so.scare@gmail.com>
2020-06-28 19:49:42 +0900

    style: Add drawer avatars

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,26 @@
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
+      clipped
       fixed
       app
     >
       <v-list>
+        <v-list-item
+          v-for="(channel, i) in channels"
+          :key="i"
+          :to="channel.to"
+          router
+          exact
+          class="px-0"
+        >
+          <v-list-item-avatar class="ml-4">
+            <img :src="channel.avatar" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ channel.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
@@ -24,8 +39,15 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-navigation-drawer clipped fixed right app>
+      <v-card>
+        <v-card-title>Description</v-card-title>
+        <v-card-text>No description</v-card-text>
+      </v-card>
+    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
+      clipped-left
+      clipped-right
       fixed
       app
       dark
@@ -45,12 +67,6 @@
       <v-spacer />
       <v-btn>
         <v-icon>mdi-checkbox-marked-outline</v-icon>
-      </v-btn>
-      <v-btn>
-        <v-icon>mdi-folder-outline</v-icon>
-      </v-btn>
-      <v-btn>
-        <v-icon>mdi-account-plus</v-icon>
       </v-btn>
       <v-btn>
         <v-icon>mdi-information-outline</v-icon>
@@ -81,6 +97,28 @@ export default {
           icon: "mdi-chart-bubble",
           title: "Inspire",
           to: "/inspire"
+        }
+      ],
+      channels: [
+        {
+          avatar: "/home.png",
+          title: "鴨川家グループ",
+          to: "/group"
+        },
+        {
+          avatar: "/duck_yellow.png",
+          title: "鴨川檸檬",
+          to: "/yellow"
+        },
+        {
+          avatar: "/duck_purple.png",
+          title: "鴨川紫苑",
+          to: "/purple"
+        },
+        {
+          avatar: "/duck_green.png",
+          title: "鴨川若菜",
+          to: "/green"
         }
       ],
       miniVariant: false,
