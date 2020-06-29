395deb59fc78fc86f6c557f1cb4f3ef450ab89df
akb <doge.wow.so.scare@gmail.com>
2020-06-29 13:43:42 +0900

    feat: Display sample message in the channel

---
+++ components/Message.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card flat tile outlined>
+    <v-card-text>
+      <v-layout justify-center align-center>
+        <v-row>
+          <v-col cols="1">
+            <v-avatar>
+              <img src="/duck_yellow.png" />
+            </v-avatar>
+          </v-col>
+          <v-col cols="11">
+            <v-card flat tile>
+              <v-card-title class="py-1 text-subtitle-2">
+                田中太郎
+                <v-spacer />
+                2020年7月1日
+              </v-card-title>
+              <v-card-text class="text-left">
+                こんにちは
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-layout>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  text-transform: none; /** Disable conversion to upper cases  */
+}
+</style>
--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -3,9 +3,13 @@
     <v-flex class="text-center">
       Viewer
       {{ this.$route.params.id }}
+      <Message />
     </v-flex>
     <v-footer absolute class="pa-0">
       <Editor />
     </v-footer>
   </v-layout>
 </template>
+<script>
+export default {};
+</script>
