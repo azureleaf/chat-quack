04b0ca84f5a69202b81f96c6e803e362bfa4f37e
akb <doge.wow.so.scare@gmail.com>
2020-06-29 13:42:24 +0900

    refactor: Create template file for the dialog box to edit the channel summary

---
+++ components/ChannelAbstractDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-dialog>
+    This will be the dialog box to edit the channel abstract
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.btn {
+  text-transform: none; /** Disable conversion to upper cases  */
+}
+</style>
