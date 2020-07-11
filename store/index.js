9984d859b1f16fd34204322acb3a4839d048a91d
akb <doge.wow.so.scare@gmail.com>
2020-07-11 12:50:53 +0900

    feat: Populate the messages with appropriate IDs

--- store/index.js
+++ store/index.js
@@ -1,44 +1,6 @@
 export const state = () => ({
   // For which user the entire UI is displayed
   viewerId: 2, // Initial value
-  // should be stored in the firebase later
-  channels: [
-    {
-      id: 1,
-      isPinned: false,
-      isPerson: false,
-      avatar: "/home.png",
-      title: "鴨川家雑談"
-    },
-    {
-      id: 2,
-      isPinned: false,
-      isPerson: true,
-      avatar: "/duck_yellow.png",
-      title: "鴨川檸檬"
-    },
-    {
-      id: 3,
-      isPinned: false,
-      isPerson: true,
-      avatar: "/duck_purple.png",
-      title: "鴨川紫苑"
-    },
-    {
-      id: 4,
-      isPinned: false,
-      isPerson: true,
-      avatar: "/duck_green.png",
-      title: "鴨川若菜"
-    },
-    {
-      id: 5,
-      isPinned: false,
-      isPerson: true,
-      avatar: "/duck_pink.png",
-      title: "鴨川桜"
-    }
-  ],
   users: [
     {
       id: 1,
@@ -64,93 +26,119 @@ export const state = () => ({
   rooms: [
     {
       id: 10001,
-      isBilateral: false,
-      avatar: "/home.png",
-      name: "鴨川家雑談",
+      type: "multilateral",
       users: [
         { id: 1, isPinning: true },
+        { id: 2, isPinning: false },
+        { id: 3, isPinning: true },
+        { id: 4, isPinning: false }
+      ],
+      avatar: "/home.png",
+      name: "鴨川家雑談"
+    },
+    {
+      id: 10002,
+      type: "bilateral",
+      users: [
+        { id: 2, isPinning: false },
+        { id: 3, isPinning: false }
+      ]
+    },
+    {
+      id: 10003,
+      type: "bilateral",
+      users: [
+        { id: 2, isPinning: false },
+        { id: 4, isPinning: false }
+      ]
+    },
+    {
+      id: 10004,
+      type: "bilateral",
+      users: [
+        { id: 1, isPinning: false },
         { id: 2, isPinning: false }
       ]
     },
     {
-      id: 10002,
-      users: [
-        { id: 2, isPinning: false },
-        { id: 3, isPinning: false }
-      ],
-      isBilateral: true
-    },
-    {
-      id: 10003,
-      users: [
-        { id: 2, isPinning: false },
-        { id: 4, isPinning: false }
-      ],
-      isBilateral: true
-    },
-    {
-      id: 10004,
-      users: [
-        { id: 1, isPinning: false },
-        { id: 2, isPinning: false }
-      ],
-      isBilateral: true
-    },
-    {
       id: 10005,
+      type: "bilateral",
       users: [
         { id: 3, isPinning: false },
         { id: 4, isPinning: false }
-      ],
-      isBilateral: true
+      ]
     },
     {
       id: 10006,
+      type: "bilateral",
       users: [
         { id: 1, isPinning: false },
         { id: 3, isPinning: false }
-      ],
-      isBilateral: true
+      ]
     },
     {
       id: 10007,
+      type: "bilateral",
       users: [
         { id: 1, isPinning: false },
         { id: 4, isPinning: false }
-      ],
-      isBilateral: true
+      ]
+    },
+    {
+      id: 10008,
+      type: "unilateral",
+      users: [{ id: 1, isPinning: true }]
+    },
+    {
+      id: 10009,
+      type: "unilateral",
+      users: [{ id: 2, isPinning: false }]
+    },
+    {
+      id: 10010,
+      type: "unilateral",
+      users: [{ id: 3, isPinning: false }]
+    },
+    {
+      id: 10011,
+      type: "unilateral",
+      users: [{ id: 4, isPinning: false }]
     }
   ],
   // should be stored in the firebase later
   msgs: [
     {
       id: 1,
-      senderId: 2,
-      channelId: null,
+      senderId: 1,
+      recipientId: null,
+      roomId: 10001,
       reaction: [],
       timestamp: "2020-06-30T05:10:33.717Z",
       text: "皆さんこんにちは。レモンです。"
     },
     {
       id: 2,
-      senderId: 2,
-      channelId: 3,
+      senderId: 1,
+      recipientId: null,
+      roomId: 10004,
       reaction: [],
       timestamp: "2020-07-03T05:10:34.717Z",
       text: "紫苑さんこんにちは。レモンです。"
     },
     {
       id: 3,
-      senderId: 3,
-      channelId: 2,
+      senderId: 2,
+      recipientId: null,
+      roomId: 10004,
       reaction: [],
       timestamp: "2020-07-03T05:12:34.717Z",
       text: "こんにちはレモンさん。"
     },
     {
       id: 4,
-      senderId: 2,
-      channelId: 4,
+      senderId: 1,
+      recipientId: null,
+      roomId: 10006,
       reaction: [],
       timestamp: "2020-06-30T05:10:33.717Z",
       text: "若菜さんこんにちは。レモンです。"
@@ -158,10 +146,20 @@ export const state = () => ({
     {
       id: 5,
       senderId: 3,
-      channelId: null,
+      recipientId: null,
+      roomId: 10001,
       reaction: [],
       timestamp: "2020-07-03T05:12:34.717Z",
       text: "若菜です。みなさんはじめまして。"
+    },
+    {
+      id: 6,
+      senderId: 1,
+      recipientId: null,
+      roomId: 10008,
+      reaction: [],
+      timestamp: "2020-07-03T05:12:34.717Z",
+      text: "レモンの買い物メモ：　牛乳を買う"
     }
   ]
 });
